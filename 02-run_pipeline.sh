0b943e6b082adf94436478b08b9583db502982ec
ShamovIA <shamovia@yandex.ru>
2021-05-25 19:25:53 +0300

    forget it

---
+++ 02-run_pipeline.sh
@@ -0,0 +1,8 @@
+python3 matcher.py configs/config_gmanga_remanga.yaml
+python3 matcher.py configs/config_gmanga_mangalib.yaml
+
+python3 matcher.py configs/config_gmint_remanga.yaml
+python3 matcher.py configs/config_gmint_mangalib.yaml
+
+python3 matcher.py configs/config_selfmanga_remanga.yaml
+python3 matcher.py configs/config_selfmanga_mangalib.yaml
