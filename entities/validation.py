afae45f16cb7ba4a98df0e74b7caf0fa4356ff5d
vaskers5 <danil.krokodil@mail.ru>
2021-07-18 01:33:34 +0300

    fixed return metrics

--- entities/validation.py
+++ entities/validation.py
@@ -11,16 +11,12 @@ FUNCTIONS = {"roc_auc_score": roc_auc_score, "f1_score": f1_score,
 class BinaryValidator:
 
     @staticmethod
-    def validate(test: pd.Series, pred: pd.Series, old_df=None) -> pd.DataFrame:
+    def validate(test: pd.Series, pred: pd.Series) -> defaultdict:
         metrics = defaultdict(list)
         for name in FUNCTIONS:
             metrics[name] = FUNCTIONS[name](test, pred)
 
-        df = pd.DataFrame(metrics, index=[0])
-        if old_df != None:
-            return pd.concat([old_df, df], ignore_index=True)
-        else:
-            return df
+        return metrics
 
 
 
