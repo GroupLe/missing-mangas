1d929c7a5c7dd799fa85c565789da62572ca829a
vaskers5 <danil.krokodil@mail.ru>
2021-07-17 20:57:21 +0300

    fixed Validator

--- entities/validation.py
+++ entities/validation.py
@@ -2,28 +2,28 @@ from sklearn.metrics import roc_auc_score, f1_score, mean_squared_error, log_los
 from itertools import combinations
 import numpy as np
 from collections import defaultdict
+import pandas as pd
 
 FUNCTIONS = {"roc_auc_score": roc_auc_score, "f1_score": f1_score,
              "mean_squared_error": mean_squared_error, "log_loss": log_loss}
 
 
 class BinaryValidator:
-    def __init__(self, test: pd.DataFrame, predict: pd.DataFrame):
-        self.predict = predict
-        self.test = test
 
-    def validate(self, path_to_csv: str) -> None:
+    @staticmethod
+    def validate(test: pd.Series, pred: pd.Series, old_df=None) -> pd.DataFrame:
         metrics = defaultdict(list)
         for name in FUNCTIONS:
-            metrics[name] = FUNCTIONS[name](self.test, self.predict)
+            metrics[name] = FUNCTIONS[name](test, pred)
 
         df = pd.DataFrame(metrics, index=[0])
-        try:
-            old_df = pd.read_csv(path_to_csv)
-            result_df = pd.concat([old_df, df], ignore_index=True)
-            result_df.to_csv(path_to_csv, index=False)
-        except:
-            df.to_csv(path_to_csv, index=False)
+        if old_df != None:
+            return pd.concat([old_df, df], ignore_index=True)
+        else:
+            return df
+
+
+
 
 
 
